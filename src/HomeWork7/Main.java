b0772b2c8cc51eec3fc6447cb8a36645d3190f66
Krell <Sora1988@gmail.com>
2022-02-02 19:15:19 +0400

    Не стал добавлять true b false по сути также выглядит

--- src/HomeWork7/Main.java
+++ src/HomeWork7/Main.java
@@ -2,24 +2,33 @@ package HomeWork7;
 
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
-        Dog dog = new Dog("Борбик", 10, "серый");
-        Dog dog1 = new Dog("Бок", 140, "черный");
-        dog.run(Animals.random());
-        dog.swim(Animals.random());
-        dog1.run(Animals.random());
-        dog1.swim(Animals.random());
-        Cat cat = new Cat("Борис", 3, "черный");
-        Cat cat1 = new Cat("Кузя", 7, "серый");
-        Cat cat2 = new Cat("Барсик", 2, "белый");
-        cat.swim(Animals.random());
-        cat.run(Animals.random());
-        cat1.run(Animals.random());
-        cat2.run(Animals.random());
-        Animals.showInfo();
-        cat.sleep();
+        Animals animals[] = {
+                new Dog("Бок", 140, "черный"),
+                new Cat("Борис", 3, "черный"),
+                new Cat("Гаврик", 7, "серый"),
+                new Cat("Барсик", 2, "белый"),
+                new Dog("Борбик", 10, "серый"),
+        };
+        Plate plate = new Plate(100);
+
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[i].run(Animals.random()));
+            System.out.println(animals[i].satietyInfo());
+            System.out.println(animals[i].eat(plate));
+            System.out.println(animals[i].satietyInfo());
+            System.out.println("Еды в тарелке осталось: " + plate.plateInfo());
+        }
+        System.out.println();
+        plate.setFoodQuantity(200);
+        System.out.println(plate.plateInfo());
+        System.out.println();
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[i].swim(Animals.random()));
+            System.out.println(animals[i].satietyInfo());
+            System.out.println(animals[i].eat(plate));
+            System.out.println(animals[i].satietyInfo());
+            System.out.println("Еды в тарелке осталось: " + plate.plateInfo());
+        }
     }
 }
